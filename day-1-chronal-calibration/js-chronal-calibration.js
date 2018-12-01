ffac8bfa3d9bc91a7f51f8b064c2915473800fdf
Michiel Koning <m.g.koning@gmail.com>
2018-12-01 21:14:38 +0100

    day 1 js version simplified: arrays have map/reduce built in

--- day-1-chronal-calibration/js-chronal-calibration.js
+++ day-1-chronal-calibration/js-chronal-calibration.js
@@ -2,9 +2,9 @@ let fs = require('fs');
 let lib = require('../js-shared/lib.js')
 
 let input = fs.readFileSync('input.txt', 'utf8');
-let frequencies = lib.toArray(lib.map(l => parseInt(l, 10), input.split('\r\n')));
+let frequencies = input.split('\r\n').map(l => parseInt(l, 10));
 
-let add = (a,b) => a + b;
+let add = (a, b) => a + b;
 
 function firstDuplicate(iterable) {
   let seen = new Set();
@@ -17,6 +17,6 @@ function firstDuplicate(iterable) {
 }
 
 console.log('Part 1');
-console.log(`${lib.reduce(add, 0, frequencies)}`);
+console.log(`${frequencies.reduce(add, 0)}`);
 console.log('Part 2');
 console.log(`${firstDuplicate(lib.accumulate(add, 0, lib.cycle(frequencies)))}`);
