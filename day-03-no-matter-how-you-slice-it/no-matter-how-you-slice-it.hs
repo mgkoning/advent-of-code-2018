856e2ac1aa2460e0b8ff00bfc41de22a3a2f2b07
Michiel Koning <m.g.koning@gmail.com>
2018-12-03 19:08:42 +0100

    day 3: faster version

--- day-03-no-matter-how-you-slice-it/no-matter-how-you-slice-it.hs
+++ day-03-no-matter-how-you-slice-it/no-matter-how-you-slice-it.hs
@@ -1,6 +1,9 @@
 import qualified Data.Text as T
 import Data.Text (Text, splitOn)
 import Data.Set (Set, fromList, member)
+import Data.Map.Strict (Map, (!))
+import qualified Data.Map.Strict as Map
+import Data.Map.Merge.Strict ( merge, zipWithMatched, preserveMissing)
 import Data.List (intersect)
 
 data Claim = Claim { claimId :: String, left :: Int, top :: Int, width :: Int, height :: Int } deriving (Eq, Show)
@@ -16,26 +19,44 @@ parseLine line =
       readFromText x = read $ T.unpack x
   in Claim (T.unpack $ T.drop 1 num) (readFromText left) (readFromText $ T.init top) (readFromText width) (readFromText height)
 
+-- only needed for previous version
 pairs :: Eq a => [a] -> [(a, a)]
 pairs [] = []
 pairs (x:xs) = [(x, y) | y <- xs] ++ pairs xs
 
-allCoords :: Claim -> [(Int, Int)]
-allCoords c = [(x, y)| x <- [left c..right c], y <- [top c..bottom c]]
-
+-- only needed for previous version
 overlap :: (Claim, Claim) -> [(Int, Int)]
 overlap (a, b)
   | right b < left a || right a < left b || bottom a < top b || bottom b < top a = []
   | otherwise = intersect (allCoords a) (allCoords b)
 
+allCoords :: Claim -> [(Int, Int)]
+allCoords c = [(x, y)| x <- [left c..right c], y <- [top c..bottom c]]
+
+claimMap :: [Claim] -> Map (Int, Int) Int
+claimMap claims = foldl1 mergeClaims $ map makeMap claims
+  where
+    makeMap = Map.fromList . (`zip` (repeat 1)). allCoords
+    mergeClaims = merge preserveMissing preserveMissing (zipWithMatched (\k -> (+)))
+
 solve = do
   claims <- map parseLine <$> lines <$> readFile "input.txt"
+  {- Previous (very inefficient) version:
   let overlaps = fromList $ concat $ map overlap $ pairs claims
   putStrLn "Part 1:"
   putStrLn $ show $ length overlaps
+  -}
+  putStrLn "Part 1 revisited:"
+  let claimedCloth = claimMap claims
+  putStrLn $ show $ length $ Map.filter (\x -> 1 < x) claimedCloth
 
+  {- Previous version:
   putStrLn "Part 2:"
   putStrLn $ claimId $ head $ filter (\c -> not $ any (`member` overlaps) (allCoords c)) claims
+  -}
+
+  putStrLn "Part 2 revisited:"
+  putStrLn $ claimId $ head $ filter (\c -> all ((==1) . (claimedCloth !)) (allCoords c)) claims
 
 {- Test stuff -}
 testClaim1 = Claim "test1" 1 3 4 4
