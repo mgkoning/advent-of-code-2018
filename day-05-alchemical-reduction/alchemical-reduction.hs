45db020d97bf22855f90a20655658fde1c5739ab
Michiel Koning <m.g.koning@gmail.com>
2018-12-05 09:42:43 +0100

    New solution day 5

--- day-05-alchemical-reduction/alchemical-reduction.hs
+++ day-05-alchemical-reduction/alchemical-reduction.hs
@@ -1,31 +1,53 @@
 import Data.Char (toUpper)
+import Data.List (sortOn)
 
-reduce polymer = reduce' polymer []
+reduce (polymer, _) = reduce' polymer ([], 0)
   where
-    reduce' (a:b:rest) reduced
-      | a /= b && toUpper a == toUpper b = reduce' rest reduced
-      | otherwise = reduce' (b:rest) (a:reduced)
-    reduce' remaining reduced = reverse $ remaining ++ reduced
+    reduce' (a:b:rest) (reduced, reducedLength)
+      | a /= b && toUpper a == toUpper b = reduce' rest (reduced, reducedLength)
+      | otherwise = reduce' (b:rest) ((a:reduced), reducedLength `seq` reducedLength + 1)
+    reduce' remaining (reduced, reducedLength) = (reverse $ remaining ++ reduced, 1 + reducedLength)
 
-fullReact polymer = untilDone $ iterate reduce polymer
+fullReact polymer = untilDone $ iterate reduce (polymer, length polymer)
   where untilDone (a:b:rest)
-          | length a == length b = a
+          | snd a == snd b = a
           | otherwise = untilDone (b:rest)
 
 optimalReduction polymer = foldl1 bestReduction $ map reactWithoutUnit ['a'..'z']
   where
-    reactWithoutUnit unit =
-      let result = fullReact $ filter (\c -> toUpper c /= toUpper unit) polymer
-      in ((result, unit), length result)
+    reactWithoutUnit unit = fullReact $ filter (\c -> toUpper c /= toUpper unit) polymer
     bestReduction a b = if snd a < snd b then a else b
+
+solveOld = do
+  putStrLn "Part 1:"
+  polymer <- readFile "input.txt"
+  let fullyReacted = fullReact polymer
+  putStrLn $ show $ snd fullyReacted
+  putStrLn "Part 2:"
+  putStrLn $ show $ optimalReduction polymer
+
+{-
+  Better version: a single pass is enough if you reduce from the right.
+  Inspired by the advent of code subreddit solutions megathread
+-}  
+
+collapse :: [Char] -> [Char]
+collapse polymer = foldr reduceStep "" polymer
+  where reduceStep a (b:reduced)
+          | a /= b && toUpper a == toUpper b = reduced
+          | otherwise = (a:b:reduced)
+        reduceStep remaining reduced = remaining : reduced
+
+optimalCollapse polymer = head $ sortOn length $ map collapseWithoutUnit ['a'..'z']
+  where collapseWithoutUnit unit = collapse $ filter (\c -> toUpper c /= toUpper unit) polymer
 
 solve = do
   putStrLn "Part 1:"
   polymer <- readFile "input.txt"
-  let fullyReacted = fullReact polymer
-  putStrLn $ show $ length fullyReacted
+  let collapsed = collapse polymer
+  putStrLn $ show $ length $ collapsed
   putStrLn "Part 2:"
-  putStrLn $ show $ optimalReduction polymer
+  putStrLn $ show $ length $ optimalCollapse polymer
 
-
+        
 testPolymer = "dabAcCaCBAcCcaDA"
