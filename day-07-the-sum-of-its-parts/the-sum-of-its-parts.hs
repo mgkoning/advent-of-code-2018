7bc6c7cecdc7297ce7cf56f84fbde6668d79514b
Michiel Koning <michiel@eyetoeye.nl>
2018-12-07 18:26:01 +0100

    day 07: bit further, added some debugging statements

--- day-07-the-sum-of-its-parts/the-sum-of-its-parts.hs
+++ day-07-the-sum-of-its-parts/the-sum-of-its-parts.hs
@@ -1,5 +1,5 @@
 import Data.Char (ord)
-import Data.List (sort, nub, (\\), delete, intersect)
+import Data.List (sort, nub, (\\), delete, intersect, partition)
 import Data.Map.Strict (Map, fromList, keys, (!), elems)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (isJust, fromJust)
@@ -13,7 +13,7 @@ getPrerequisites steps key = map fst $ filter ((==key) . snd) steps
 
 getPrerequisiteMap steps = let keys = map snd steps in fromList $ zip keys $ map (getPrerequisites steps) keys
 
-time base stepChar = base + 1 + (ord stepChar - ord 'A')
+timeForStep base stepChar = base + 1 + (ord stepChar - ord 'A')
 
 properOrder steps = determineOrder prerequisiteMap startAt []
   where startAt = noPrerequisite steps
@@ -24,31 +24,35 @@ properOrder steps = determineOrder prerequisiteMap startAt []
                 newAvailable = keys $ Map.filter null removedPrereq
                 newMap = Map.filterWithKey (\k _ -> not $ k `elem` newAvailable) removedPrereq
 
-{- Since we're not working efficiently, this is too fast -}
+{- Since we're not working efficiently, this is too fast
 totalTime baseTime steps = (criticalPath ! (head $ reverse order))
   where order = properOrder steps
         criticalPath = foldl updateLongestPathMap Map.empty order
         updateLongestPathMap pathMap next =
           let prerequisites = getPrerequisites steps next
               timeNeeded = (time baseTime next) + (maximum $ [0] ++ map (pathMap !) prerequisites)
-          in  Map.insert next timeNeeded pathMap
+          in  Map.insert next timeNeeded pathMap -}
 
-timeNeeded baseTime steps workers = timeNeeded' (drop 1 order) [] (take 1 order) (initWorkersAvailable workers) [0..]
+timeNeeded baseTime steps workers = timeNeeded' (drop 1 order) [] (take 1 order) (take workers [0..]) [] [0..] []
   where order = properOrder steps
         prerequisiteMap = getPrerequisiteMap steps
         initWorkersAvailable n = fromList $ map initWorker $ take n [0..]
         initWorker n = (n, (0, Nothing))
-        timeNeeded' [] _ [] _ (time:_) = time
-        timeNeeded' remaining done available workers (time:moreTime) =
-          let doneWorkers = Map.filter isDone workers
-              doneSteps = elems $ Map.map (fromJust . snd) doneWorkers
-              isDone (_, Nothing) = False
-              isDone (t, Just a) = t <= time
+        timeNeeded' :: [Char] -> [Char] -> [Char] -> [Int] -> [(Int, Char, Int)] -> [Int] -> [(Int, [(Int, Char, Int)])] -> (Int, [(Int, [(Int, Char, Int)])])
+        timeNeeded' [] _ [] _ _ (time:_) states = (time, reverse states)
+        timeNeeded' remaining done available idleWorkers busyWorkers (time:moreTime) states =
+          let (doneWorkers, stillBusyWorkers) = partition isDone busyWorkers
+              doneSteps = map (\(_, a, _) -> a) doneWorkers
+              isDone (id, a, t) = t < time
               newDone = done ++ doneSteps
               nowAvailable = sort ((findNowAvailable newDone remaining prerequisiteMap) ++ available)
-              newWorkers = foldl markIdle workers (keys doneWorkers)
-              markIdle m k = Map.adjust (const (0, Nothing)) k m
-          in if null nowAvailable then timeNeeded' remaining newDone nowAvailable newWorkers moreTime else 0
+              newIdleWorkers = idleWorkers ++ (map (\(a, _, _) -> a) doneWorkers)
+              toQueue = min (length nowAvailable) (length newIdleWorkers)
+              stepsToQueue = take toQueue nowAvailable
+              newRemaining = remaining \\ doneSteps
+              queuedWorkers = zip3 (take toQueue newIdleWorkers) stepsToQueue (map ((+ (-1)) . (+time) . (timeForStep baseTime)) stepsToQueue)
+              newBusyWorkers = (stillBusyWorkers ++ queuedWorkers)
+          in timeNeeded' newRemaining newDone (drop toQueue nowAvailable) (drop toQueue newIdleWorkers) (newBusyWorkers) moreTime ((time, newBusyWorkers):states)
 
 findNowAvailable :: [Char] -> [Char] -> Map Char [Char] -> [Char]
 findNowAvailable done remaining prerequisiteMap = intersect remaining $ keys $ Map.filter (isAvailable done) prerequisiteMap
@@ -59,6 +63,6 @@ solve = do
   putStrLn "Part 1:"
   putStrLn $ properOrder steps
   putStrLn "Part 2:"
-  putStrLn $ show $ totalTime 60 steps
+  --putStrLn $ show $ 1 + (timeNeeded 60 steps 5)
 
 testInput = [('C', 'A'), ('C', 'F'), ('A', 'B'), ('A', 'D'), ('B', 'E'), ('D', 'E'), ('F', 'E')]
