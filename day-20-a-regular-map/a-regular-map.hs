8ecb24b9e2df3c8bc103ec47e05d8ddd50f4ae80
Michiel Koning <m.g.koning@gmail.com>
2018-12-20 20:16:25 +0100

    Day 20: Part 1 & 2 done

--- day-20-a-regular-map/a-regular-map.hs
+++ day-20-a-regular-map/a-regular-map.hs
@@ -1,28 +1,45 @@
-import Data.Map.Strict (Map, insertWith)
-import qualified Data.Map.Strict as Map
+import Data.HashMap.Strict (insertWith)
+import qualified Data.HashMap.Strict as Map
+import Data.List (foldl', sortOn, groupBy)
+import Data.Function (on)
 
-buildMap s = move (drop 1 s) [((0,0), 0)] Map.empty
-  where move [] c mapSoFar = (mapSoFar, [])
+start = ((0, 0), 0) :: ((Int, Int), Int)
+
+buildMap s = move (dropWhile (=='^') s) [start] (Map.fromList [start])
+  where move [] c mapSoFar = (mapSoFar, c, [])
         move (d:directions) currentPositions mapSoFar
-          | d == '$' = (mapSoFar, directions)
+          | d == '$' = (mapSoFar, currentPositions, directions)
           | d `elem` "NEWS" = let newPositions = map (step d) currentPositions
-                              in move directions newPositions (updateMap mapSoFar newPositions)
+                                  m' = (updateMap mapSoFar newPositions)
+                              in move directions newPositions m'
           | d == '(' = let (newPositions, remainingDirections, childrenMap) = followChildren directions currentPositions [] mapSoFar
-                       in move remainingDirections newPositions childrenMap
-        followChildren directions startingPositions positions mapSoFar = 
-          let 1 = 1 in (positions, directions, mapSoFar)
-          -- | d == ')' = (positions, directions, mapSoFar)
-          -- | d == '|' = 
-        updateMap = foldr (\(pos, l) m -> insertWith min pos l m)
-        step d (p, l) = let l' = l+1; p' = case d of 'N' -> up p; 'S' -> down p; 'W' -> left p; 'E' -> right p in l' `seq` (p', l')
+                       in move remainingDirections (uniquify newPositions) childrenMap
+          | otherwise = (mapSoFar, currentPositions, (d:directions))
+        followChildren directions startingPositions positions mapSoFar =
+          let (m', pos', d:ds) = move directions startingPositions mapSoFar
+          in case d of
+               ')' -> (pos' ++ positions, ds, m')
+               '|' -> followChildren ds startingPositions (pos' ++ positions) m'
+        updateMap = foldl' (\m (pos, l) -> insertWith min pos l m) 
+        step d (p, l) = let p' = case d of 'N' -> up p; 'S' -> down p; 'W' -> left p; 'E' -> right p in (p', l+1)
 
-add (x, y) (dx, dy) = let x' = x + dx; y' = y + dy in x' `seq` y' `seq` (x', y')
+uniquify positions = map (head . sortOn snd) $ groupBy ((==) `on` fst) $ sortOn fst positions
+
+add (x, y) (dx, dy) = (x + dx,  y + dy)
 up = (`add` (0, 1))
 down = (`add` (0, -1))
 left = (`add` (-1, 0))
 right = (`add` (1, 0))
 
+part1 = Map.foldl' max 0
+
+part2 = length . filter ((>=1000) . snd) . Map.toList
+
 solve = do
-  (builtMap, _) <- buildMap <$> readFile "input.txt"
+  (winner, _, _) <- buildMap <$> readFile "input.txt"
   putStrLn "Part 1:"
-  print $ Map.foldl max 0 builtMap
+  print $ part1 winner
+  putStrLn "Part 2:"
+  print $ part2 winner
+
+testPart1 s = let (m, _, _) = buildMap s in part1 m
