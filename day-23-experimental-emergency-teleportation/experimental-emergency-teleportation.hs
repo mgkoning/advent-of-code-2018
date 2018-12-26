b70fc0cc2b2b33a4e15f50ae9f53acff01fabee5
Michiel Koning <m.g.koning@gmail.com>
2018-12-26 13:40:27 +0100

    Day 23: Rewrite with a correct algorithm that's pretty fast

--- day-23-experimental-emergency-teleportation/experimental-emergency-teleportation.hs
+++ day-23-experimental-emergency-teleportation/experimental-emergency-teleportation.hs
@@ -1,22 +1,25 @@
-import qualified Data.HashMap.Strict as HashMap
-import Data.List (sortOn, subsequences, maximumBy)
-import Data.Ord (comparing)
-import Data.Tuple (swap)
-import qualified Data.Set as Set
+import Data.List (sortOn, foldl')
 import Text.ParserCombinators.Parsec (Parser, sepBy, getInput, setInput, (<|>), sepEndBy, parse)
 import Text.ParserCombinators.Parsec.Char (char, string)
 import Numeric (readDec, readSigned)
 import Control.Applicative (empty)
 
-import Data.List (mapAccumL)
-import qualified Data.IntSet as S
-import qualified Data.Vector as V
+type Vector3 = (Int, Int, Int)
 
-newtype Vector3 = Vector3 (Int, Int, Int) deriving (Show, Eq, Ord)
+origin = (0, 0, 0)
 
-origin = Vector3 (0, 0, 0)
+data Nanobot = Nanobot { botId :: Int, pos :: Vector3, radius :: Int} deriving (Show, Eq, Ord)
 
-data Nanobot = Nanobot { id :: Int, pos :: Vector3, radius :: Int} deriving (Show, Eq, Ord)
+data SearchCube = SearchCube { botCount :: Int, originDistance :: Int, size :: Int, minCorner :: Vector3 } deriving (Show, Eq, Ord)
+
+betterCube (SearchCube botsA distanceA sizeA _) (SearchCube botsB distanceB sizeB _) =
+  case compare botsA botsB of
+    EQ -> case compare distanceA distanceB of
+            EQ -> sizeA < sizeB
+            LT -> True
+            GT -> False
+    LT -> False
+    GT -> True
 
 inRangeCount nanobots = length $ filter (inRange baddestBot) nanobots
   where baddestBot = head $ reverse $ sortOn radius nanobots
@@ -24,30 +27,55 @@ inRangeCount nanobots = length $ filter (inRange baddestBot) nanobots
 
 inRange (Nanobot _ p1 radius) (Nanobot _ p2 _) = distance p1 p2 <= radius
 
-distance (Vector3 (x, y, z)) (Vector3 (x', y', z')) = sum [abs $ x - x', abs $ y - y', abs $ z - z']
+distance (x, y, z) (x', y', z') = sum [abs $ x - x', abs $ y - y', abs $ z - z']
 
-overlaps (Nanobot _ v1 r1) (Nanobot _ v2 r2) = distance v1 v2 <= (r1 + r2)
-
-pairs :: [a] -> [(a, a)]
-pairs [] = []
-pairs (x:xs) = [(x, y) | y <- xs] ++ pairs xs
-
-{- "Inspired" by a discussion of the problem on the subreddit. I was stuck on this for so many
+{- "Inspired" by several threads about this problem on the subreddit. I was stuck on this for so many
    hours I sort of gave up.
-   From that discussion also: it seems this may not actually give the correct answer for all inputs.
+   Essentially, what happens here is we find the coordinate by iterated subdivision of the space into cubes
+   until the 'best' cube so far has size 1. That cube is the winner.
+   We start out with a cube big enough to touch all bots, and continually subdivide into 8 subcubes until we're done.
 -}
-part2 nanobots = minDistance possibleAnswer
-  where cliques = getMaximalCliques overlaps nanobots
-        maximalClique = maximumBy (comparing length) cliques
-        possibleAnswer = maximumBy (comparing minDistance) maximalClique
-        minDistance n@(Nanobot _ v r) = distance v origin - r
+part2 nanobots = let (SearchCube botCount originDistance _ pos, _) = findBestCoordinate [initialCube] in (originDistance, botCount, pos)
+  where allBots = length nanobots 
+        initialCube = head $ dropWhile ((<allBots) . botCount) $ map makeInitialCube $ [1..]
+        makeInitialCube powerOfTwo = let size = 2^powerOfTwo
+                                         halfSize = 2^(powerOfTwo-1)
+                                     in buildCube nanobots size (-halfSize, -halfSize, -halfSize)
+        findBestCoordinate (c:cubes)
+          | (size c) == 1 = (c, cubes)
+          | otherwise = findBestCoordinate newQueue
+          where subCubes = split nanobots c
+                newQueue = foldl' sortedInsert cubes subCubes
+        sortedInsert list b = let (prefix, suffix) = break (betterCube b) list in prefix ++ (b:suffix)
+
+split nanobots (SearchCube _ _ size minCorner) =
+  map (buildCube nanobots halfSize) (newCorners minCorner halfSize)
+  where halfSize = size `div` 2
+        newCorners (x, y, z) halfSize = [(x', y', z') | x' <- [x, x + halfSize], y' <- [y, y + halfSize], z' <- [z, z + halfSize]]
+
+buildCube nanobots size pos =
+  let botCount = countBots nanobots pos (opposite pos size)
+      originDistance = distance origin cubeCenter
+      cubeCenter = let (x, y, z) = pos
+                       halfSize = size `div` 2
+                   in (x + halfSize, y + halfSize, z + halfSize)
+  in SearchCube botCount originDistance size pos
+
+countBots bots minCorner maxCorner = length $ filter (overlapsCube minCorner maxCorner) bots
+
+overlapsCube (minX, minY, minZ) (maxX, maxY, maxZ) (Nanobot _ (botX, botY, botZ) radius) =
+  (axisDistance botX minX maxX) + (axisDistance botY minY maxY) + (axisDistance botZ minZ maxZ) <= radius
+  where axisDistance x min max = let max' = max - 1 in if x < min then min - x else if max' < x then x - max' else 0
+
+opposite (x, y, z) size = (x + size, y + size, z + size)
 
 solve = do
-  nanobots <- parseNanobots <$> readFile "input.txt"
+  nanobots <- parseNanobots <$> readFile "input2.txt"
   putStrLn "Part 1:"
   print $ inRangeCount nanobots
   putStrLn "Part 2:"
-  print $ part2 nanobots
+  let (originDistance, botCount, pos) = part2 nanobots
+  putStrLn $ (show originDistance) ++ " (" ++ (show botCount) ++ " bots at " ++ (show pos) ++ ")"
 
 parseNanobots input = zipWith toNanobot [0..] posRadius
   where toNanobot id (pos, radius) = Nanobot id pos radius
@@ -63,7 +91,7 @@ parsePosRadius = (,) <$ string "pos=<" <*> parseVector3 <* string ">, r=" <*> pa
 
 parseVector3 :: Parser Vector3
 parseVector3 = mkVector3 <$> parseInt `sepBy` (char ',')
-  where mkVector3 (a:b:c:[]) = Vector3 (a,b,c)
+  where mkVector3 (a:b:c:[]) = (a,b,c)
 
 parseInt :: Parser Int
 parseInt = do s <- getInput
@@ -78,33 +106,3 @@ testNanobots@(n1:n2:n3:n4:n5:n6:[]) = parseNanobots $ unlines [
   "pos=<14,14,14>, r=6",
   "pos=<50,50,50>, r=200",
   "pos=<10,10,10>, r=5"]
-
-
-
-{- Used this code from a haskell package for MaximalCliques: -}
-
--- | Given a list of nodes, and a function that determines whether there is an edge between any two nodes, yields a list of maximal 
--- cliques -- sets of nodes such that every node is connected to every other, and such that no other node may be added while maintaining this property.
-getMaximalCliques :: (a -> a -> Bool) -> [a] -> [[a]]
-getMaximalCliques tolFun xs = map (map (fst . (V.!) lv) . S.toList) $
-                              maximalCliques pickpivot (snd . ((V.!) lv)) (S.fromList $ map fst lnodes)
-    where lnodes = zip [0..] xs
-          lnodes' = map (\(k,n) -> (n,S.fromList $ filter (/=k) $ map fst $ filter (tolFun n . snd) lnodes)) lnodes
-          lv = V.fromList lnodes'
-          pickpivot p x = head $ S.elems p ++ S.elems x
-
--- | The Bron-Kerbosch algorithm for finding all maximal cliques in an undirected graph.
--- <http://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm>. Works on nodes represented as 'Int's.
-maximalCliques :: (S.IntSet -> S.IntSet -> Int) -- ^ A function that given two 'IntSet's, chooses a member of one as a pivot.
-               -> (Int -> S.IntSet)  -- ^ A function that given a node id, yields the set of its neighbors.
-               -> S.IntSet -- ^ The set of all nodes in the graph.
-               -> [S.IntSet] -- ^ An enumeration of all maximal cliques in the graph.
-maximalCliques pickpivot neighborsOf nodeset = go S.empty nodeset S.empty
-    where go r p x
-              | S.null p && S.null x = [r]
-              | otherwise =
-                  let pivot = pickpivot p x
-                      step' (p',x') v =
-                          let nv  = neighborsOf v
-                          in ((S.delete v p', S.insert v x'), go (S.insert v r) (S.intersection nv p') (S.intersection nv x'))
-                  in concat . snd $ mapAccumL step' (p,x) $ S.elems (p S.\\ neighborsOf pivot)
